051a66e3b9c8e95ac3cc127980c66e6b217ad052
Nick McNutt <nmcnutt@utk.edu>
2017-02-10 04:55:11 -0500

    Remove allocate_clusters

--- src/clusters.jl
+++ src/clusters.jl
@@ -1,4 +1,4 @@
-allocate_clusters(indices::Indices, atom_type_counts::Dict{Symbol, Int}) = Dict(Pair{Symbol, Clusters}[t => Clusters(n, indices) for (t, n) in atom_type_counts])
+#allocate_clusters(indices::Indices, atom_type_counts::Dict{Symbol, Int}) = Dict(Pair{Symbol, Clusters}[t => Clusters(n, indices) for (t, n) in atom_type_counts])
 
 type Cluster{T<:Real}
     id::AtomID

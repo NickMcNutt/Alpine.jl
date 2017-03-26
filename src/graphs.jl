b3ff2da2210c0886581cf6cb139d958ea6af35c2
Nick McNutt <nmcnutt@utk.edu>
2017-03-26 10:39:32 -0400

    Add bond functionality

--- src/graphs.jl
+++ src/graphs.jl
@@ -39,3 +39,55 @@ reversal_invariant(v) = hash(v) ⊻ hash(reverse(v))
 unique_paths{T}(paths::Vector{Dict{Vector{T}, Vector{T}}}) = (p -> unique(reversal_invariant, p)).(collect.(keys.(paths)))
 
 acyclic_paths{T}(G::Graph{T}, max_path_length::Int = 20) = unique_paths(acyclic_path_table(G, max_path_length))
+
+function generate_bonds{T}(atoms::Atoms, bond_types::Dict{Tuple{Symbol, Symbol}, Tuple{T, T}})
+    vertices = Set{Atom}()
+    edges = Vector{Tuple{Atom, Atom}}()
+    
+    coords = atoms[:coords]
+    types = atoms[:type]
+    
+    n = size(coords, 2)::Int
+    
+    for i in 2:n, j in 1:i-1
+        type_i, type_j = types[i]::Symbol, types[j]::Symbol
+        
+        dist_min, dist_max = get(bond_types, (type_i, type_j), get(bond_types, (type_j, type_i), (Inf, -Inf)))
+        if dist_min != Inf
+            if dist_min^2 <= distancesq(Inf, coords, i, j) <= dist_max^2
+                push!(vertices, atoms[i], atoms[j])
+                push!(edges, (atoms[i], atoms[j]))
+            end
+        end
+    end
+    
+    return Graph(collect(vertices), edges)
+end
+
+function unique_upto_reversal{T}(v::Vector{Vector{T}})
+    unique = Set{Vector{T}}()
+    for a in v
+        if a ∉ unique && reverse(a) ∉ unique
+            push!(unique, a)
+        end
+    end
+    collect(unique)
+end
+
+function unique_neighborhoods(path_table::Vector{Dict{Vector{Atom}, Vector{Atom}}})
+    neighborhoods = Dict{Symbol, Dict{Vector{Symbol}, Vector{Atom}}}()
+    for (atom, neighbors) in path_table[1]
+        atom_type = atom[1][:type]
+        neighborhood = sort(Atoms(neighbors)[:type])
+        push!(get!(get!(neighborhoods, atom_type, Dict{Vector{Symbol}, Vector{Atom}}()), neighborhood, Symbol[]), atom[1])
+    end
+    neighborhoods
+end
+
+function neighborhood_type_ids(neighborhoods)
+    Dict(
+        atom_type => Dict(
+            first(neighborhood_type) => i
+        for (i, neighborhood_type) in enumerate(neighborhood_types))
+    for (atom_type, neighborhood_types) in neighborhoods)
+end
